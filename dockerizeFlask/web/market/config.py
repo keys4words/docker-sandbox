df2bb2475c386ff36bf9235d880b247ed23af1f9
keys4words <keys4words@gmail.com>
2022-03-27 10:54:08 +0300

    update docker-compose in dockerizeFlask

--- dockerizeFlask/web/market/config.py
+++ dockerizeFlask/web/market/config.py
@@ -1,9 +1,12 @@
+import os
+
+
 DEV_DB = 'sqlite:///market.db'
 
-pg_user = 'admin'
-pg_pass = 'admin'
-pg_db = 'market'
-pg_host = 'localhost'
-pg_port = 5432
+pg_user = os.environ.get("POSTGRES_USER")
+pg_pass = os.environ.get("POSTGRES_PASS")
+pg_db = os.environ.get("POSTGRES_DB")
+pg_host = 'db'
+pg_port = 5433
 
 PROD_DB = f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}'
