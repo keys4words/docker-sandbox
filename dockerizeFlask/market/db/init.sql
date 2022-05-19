dfa5b04ad89d15a2190c86f40cb7239f66401f95
keys4words <keys4words@gmail.com>
2022-05-20 00:40:51 +0300

    update market compose

--- dockerizeFlask/market/db/init.sql
+++ dockerizeFlask/market/db/init.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 CREATE TABLE users (
-	id INTEGER NOT NULL, 
+	id SERIAL, 
 	username VARCHAR(30) NOT NULL, 
 	email_address VARCHAR(50) NOT NULL, 
 	password_hash VARCHAR(60) NOT NULL, 
@@ -9,14 +9,10 @@ CREATE TABLE users (
 	UNIQUE (username), 
 	UNIQUE (email_address)
 );
-INSERT INTO users VALUES(1,'jsc4','jsc4@gmail.com','$2b$12$zov8HrFAcSf3leZzMeug9OfB32ML1DRmU5OFwTDarh4rLMQiXUqVm',1000);
-INSERT INTO users VALUES(2,'jsc6','jsc6@gmail.com','$2b$12$sPaDw4yoIGR6ew9ff.7G6uhEYPlV5u1ktukEKw4VeLaZjXn88C2IO',200);
-INSERT INTO users VALUES(3,'jsc7','jsc7@gmail.com','$2b$12$7pvpGUiBmEvwXFPohgu07uS8Li7hkzuPG2ZKy4egtc0QaSM3NdbhG',1000);
-INSERT INTO users VALUES(4,'jsc8','jsc8@gmail.com','$2b$12$k09QPy4EEdjy/jyIvAvLV.ibxtMGFWnRzHgGTlc5TE2gD6u5Z5ssm',0);
-INSERT INTO users VALUES(5,'admin','admin@mail.com','$2b$12$Um3PHC5RXxsXHtbQ7GnPIewOv8o51LT7am1LcwPA80R.7eMpQ.yK2',200);
-INSERT INTO users VALUES(6,'tester','tester@mail.com','$2b$12$.GGK2fVoseielMdLDiQMyOpkm60Xs8ql4TtxNsPfthoKKXF2T5mHy',101);
+INSERT INTO users(username, email_address, password_hash, budget) VALUES('admin','admin@mail.com','$2b$12$Um3PHC5RXxsXHtbQ7GnPIewOv8o51LT7am1LcwPA80R.7eMpQ.yK2',200);
+INSERT INTO users(username, email_address, password_hash, budget) VALUES('tester','tester@mail.com','$2b$12$.GGK2fVoseielMdLDiQMyOpkm60Xs8ql4TtxNsPfthoKKXF2T5mHy',101);
 CREATE TABLE item (
-	id INTEGER NOT NULL, 
+	id SERIAL, 
 	name VARCHAR(30) NOT NULL, 
 	price INTEGER NOT NULL, 
 	barcode VARCHAR(12) NOT NULL, 
@@ -28,10 +24,10 @@ CREATE TABLE item (
 	UNIQUE (description), 
 	FOREIGN KEY(owner) REFERENCES users (id)
 );
-INSERT INTO item VALUES(1,'Iphone 10',800,'123456789123','description',6);
-INSERT INTO item VALUES(2,'Laptop',1000,'654321789123','description2',5);
-INSERT INTO item VALUES(3,'IPhone 12',899,'123123123123','Apple iPhone 12 Pro, 128GB, Graphite - Unlocked (Renewed Premium)',NULL);
-INSERT INTO item VALUES(4,'Samsung Galaxy S22',1200,'124124124124','SAMSUNG Galaxy S22 Smartphone, Factory Unlocked Android Cell Phone, 256GB, 8K Camera & Video, Brightest Display, Long Battery Life, Fast 4nm Processor, US Version, Green',NULL);
-INSERT INTO item VALUES(5,'IPhone 12 Pro',899,'125125125125','Apple iPhone 12 Pro, 256GB, Pacific Blue - Unlocked (Renewed Premium)',NULL);
-INSERT INTO item VALUES(6,'IPhone 11 Pro',799,'126126126126','Apple iPhone 11 Pro, 256GB, Midnight Green - Fully Unlocked (Renewed Premium)',NULL);
+INSERT INTO item(name, price, barcode, description, owner) VALUES('Iphone 10',800,'123456789123','description',1);
+INSERT INTO item(name, price, barcode, description, owner) VALUES('Laptop',1000,'654321789123','description2',2);
+INSERT INTO item(name, price, barcode, description, owner) VALUES('IPhone 12',899,'123123123123','Apple iPhone 12 Pro, 128GB, Graphite - Unlocked (Renewed Premium)',NULL);
+INSERT INTO item(name, price, barcode, description, owner) VALUES('Samsung Galaxy S22',1200,'124124124124','SAMSUNG Galaxy S22 Smartphone, Factory Unlocked Android Cell Phone, 256GB, 8K Camera & Video, Brightest Display, Long Battery Life, Fast 4nm Processor, US Version, Green',NULL);
+INSERT INTO item(name, price, barcode, description, owner) VALUES('IPhone 12 Pro',899,'125125125125','Apple iPhone 12 Pro, 256GB, Pacific Blue - Unlocked (Renewed Premium)',NULL);
+INSERT INTO item(name, price, barcode, description, owner) VALUES('IPhone 11 Pro',799,'126126126126','Apple iPhone 11 Pro, 256GB, Midnight Green - Fully Unlocked (Renewed Premium)',NULL);
 COMMIT;
