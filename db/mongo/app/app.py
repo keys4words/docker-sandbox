703678ea31619a4f323abb5b54b2e44b8d293b4f
keys4words <keys4words@gmail.com>
2022-10-06 21:03:53 +0300

    add python client with pymongo to mongo

--- db/mongo/app/app.py
+++ db/mongo/app/app.py
@@ -1,10 +1,16 @@
 from pymongo import MongoClient
+# from bson.objectid import ObjectId
 import pprint
 
 
-# client = MongoClient("mongodb://root:example@mongo:27017")
-with MongoClient("mongodb://root:example@localhost:27017/?authSource=main") as client:
-    db = client.get_database("main")
-    print(db.list_connection_names())
-    # for item in db.main.find():
-        # print(item)
+# client = MongoClient("mongodb://root:example@localhost:27017/?authSource=main")
+with MongoClient("mongodb://root:example@mongo:27017/?authSource=main") as client:
+    # print(client.list_database_names())
+    db = client["main"]
+    # print(db.list_collection_names())
+    collectionItems = db["items"]    
+    for item in collectionItems.find():
+        pprint.pprint(item)
+
+    result = collectionItems.find_one({ "name": "Mike"}, {"age": 1, "_id": 0})
+    print("======", result)
